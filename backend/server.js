11f4e9ff2f6a17605d6b6c566adc7dfc8ca1826e
varekaiwork <serhiisaviukwork@gmail.com>
2021-07-08 20:21:56 +0300

    setup server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const connectDB = require('./config/db');
+
+connectDB();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
